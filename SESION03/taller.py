56b99bc64e2a199ee5e675a84f0262eaf4922c72
andrftapiasba <andrftapiasba@unimonserrate.edu.co>
2024-08-28 20:04:28 -0500

    Adelanto 1
    Atributos insertados

--- SESION03/taller.py
+++ SESION03/taller.py
@@ -1,5 +1,17 @@
-'''
-Crear un programa que permita crear objetos de la clase CuentaBancaria, cuyos atributos son: numeroCta, nombreCliente, saldoCta, fechaApertura, ultimoRetiro, ultimaConsignacion.
-Aplicar encapsulamiento y definir los métodos apropiados para controlar y gestionar los atributos de los objetos creados.
-Utilizar un menu para las diferentes opciones, tales como aperturaCta, consignar, retirar y transferencia entre otros.
-'''
+class CuentaBancaria:  
+  
+  def _init_(self,numeroCta,nombreCliente, saldoCta,fechaApertura,ultimoRetiro,ultimaConsignacion):
+    self.__numeroCta= numeroCta
+    self.__nombreCliente=nombreCliente
+    self.__saldoCta= saldoCta
+    self.__fechaApertura=fechaApertura
+    self.__ultimoRetiro=ultimoRetiro
+    self.__ultimaConsignacion=ultimaConsignacion
+    
+  def set(self,numeroCta,nombreCliente,saldoCta,fechaApertura,ultimoRetiro,ultimaConsignacion):
+    self.__numeroCta=numeroCta
+    self.__nombreCliente=nombreCliente
+    self.__saldoCta= saldoCta
+    self.__fechaApertura=fechaApertura
+    self.__ultimoRetiro=ultimoRetiro
+    self.__ultimaConsignacion=ultimaConsignacion
